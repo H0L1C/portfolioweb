22098f40e7528f463cf34dc0015f40c8f6d65e7e
H0L1C <yurato0625@outlook.com>
2020-10-26 18:08:53 +0900

    PWA support

---
+++ docs/precache-manifest.e13d7856b06de0d72acb107d990b79a7.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfd785815b755161b745ca6684f42e32",
+    "url": "/404.html"
+  },
+  {
+    "revision": "120514649f16bc25fcec",
+    "url": "/css/app.25114794.css"
+  },
+  {
+    "revision": "ba43370b484c84e86cd3f56723413cbc",
+    "url": "/img/Gallery_thumbnail.ba43370b.png"
+  },
+  {
+    "revision": "485bcfe3b9bcc7edf9de11a3096f2eb6",
+    "url": "/img/HABAKILogo.485bcfe3.png"
+  },
+  {
+    "revision": "a1bac407ef18c31675f6911e852ac168",
+    "url": "/img/HABAKI_anim.a1bac407.gif"
+  },
+  {
+    "revision": "e1658a8d46b5805e422eb611155e052a",
+    "url": "/img/HABAKI_thumbnail.e1658a8d.png"
+  },
+  {
+    "revision": "65a9cbc42c91244203486c8cbf8fc449",
+    "url": "/img/HABAKI_web.65a9cbc4.png"
+  },
+  {
+    "revision": "cfeaa0e95afa3a8a62dd9da4253a8aae",
+    "url": "/img/KOUSA_thumbnail.cfeaa0e9.png"
+  },
+  {
+    "revision": "5793710b0d08741d3a4f7181ac874333",
+    "url": "/img/OGP.png"
+  },
+  {
+    "revision": "27f3a81f68faf62559b9e491451c3927",
+    "url": "/img/PICTICE_thumbnail.27f3a81f.png"
+  },
+  {
+    "revision": "5dbd0ce1324adba4ee9c11fcda06f4c9",
+    "url": "/img/PictIceConcept.5dbd0ce1.png"
+  },
+  {
+    "revision": "5163b73c58b653eb402bda59b83c06a9",
+    "url": "/img/PictIceWeb.5163b73c.png"
+  },
+  {
+    "revision": "6be0714e7d53e6241da4345b0eca64ca",
+    "url": "/img/music_movie.6be0714e.png"
+  },
+  {
+    "revision": "1231b68e5590fdf509b9a4ce18c27bf1",
+    "url": "/img/myLogo.1231b68e.svg"
+  },
+  {
+    "revision": "70ffb8944eb7a003f93af304a16cd6a7",
+    "url": "/img/peta.70ffb894.gif"
+  },
+  {
+    "revision": "078a1aff25d6bb65c6c3965ebe5820b2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "120514649f16bc25fcec",
+    "url": "/js/app.ff41fb4e.js"
+  },
+  {
+    "revision": "2e2dd40d43a0c0df8faf",
+    "url": "/js/chunk-vendors.25c9d5b8.js"
+  },
+  {
+    "revision": "a980470261771e64009709b4b4390b82",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
